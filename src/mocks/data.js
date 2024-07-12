10e8980fdce05202d85f339d0c08b9ca9f134d66
Heloisa <helo.amxavier@gmail.com>
2024-07-12 14:35:00 -0300

    feat: add msw to mock data

---
+++ src/mocks/data.js
@@ -0,0 +1,17 @@
+export const listClientMock = [
+    {
+        id: 1,
+        client: 1,
+        description: "Bring coffee"
+    },
+    {
+        id: 2,
+        client: 1,
+        description: "Focus on javascript"
+    },
+    {
+        id: 2,
+        client: 1,
+        description: "Learn to cook"
+    },
+];
---
+++ src/mocks/handlers.js
@@ -0,0 +1,48 @@
+import {http, HttpResponse} from 'msw';
+import {listClientMock} from './data';
+
+const allClients = new Map(Object.entries(listClientMock));
+
+export const handlers = [
+    http.get('/api/clients', () => {
+        return HttpResponse.json({
+            id: 1,
+            name: 'ansons',
+        });
+    }),
+
+    http.post('/api/clients', async ({request}) => {
+        const requestBody = await request.json();
+        return HttpResponse.json(
+            {
+                content: requestBody,
+                createdAt: new Date().toLocaleString(),
+            },
+            {status: 201}
+        );
+    }),
+
+    http.put('/api/clients', async ({request}) => {
+        const requestBody = await request.json();
+        return HttpResponse.json(
+            {
+                content: requestBody,
+            },
+            {status: 200}
+        );
+    }),
+
+    http.delete('/api/clients/:id', ({params}) => {
+        const {id} = params;
+
+        const deletedClient = allClients.get(id.toString());
+
+        if (!deletedClient) {
+            return new HttpResponse(null, {status: 404});
+        }
+
+        allClients.delete(id.toString());
+
+        return HttpResponse.json(deletedClient);
+    })
+];
